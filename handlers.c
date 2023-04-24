20afd864607f1f3a8290e61434c61fe827ef7c38
fieldb02 <bonyango118@gmail.com>
2023-04-24 23:09:11 +0300

    Done

--- handlers.c
+++ handlers.c
@@ -101,33 +101,27 @@ int write_num(int ind, char buffer[], int flags,
 {
 	int i, padd_start = 1;
 
-	if (prec == 0 && ind == BUFF_SIZE -
-			2 && buffer[ind] == '0' && width == 0)
-		return (0); /* printf(".0d", 0)  no char is printed */
 	if (prec == 0 && ind == BUFF_SIZE - 2 && buffer[ind] == '0')
-		buffer[ind] = padd = ' '; /* width is displayed with padding ' ' */
-	if (prec > 0 && prec < length)
-		padd = ' ';
+		return width > 0 ? (buffer[ind] = padd = ' ',
+				write(1, &buffer[ind], width)) : 0;
 	while (prec > length)
 		buffer[--ind] = '0', length++;
 	if (extra_c != 0)
 		length++;
 	if (width > length)
 	{
-		for (i = 1; i < width - length + 1; i++)
-			buffer[i] = padd;
-		buffer[i] = '\0';
+		i = width - length + 1;
 		if (flags & F_MINUS && padd == ' ') /* Asign extra char to left of buffer */
 		{
 			if (extra_c)
 				buffer[--ind] = extra_c;
-			return (write(1, &buffer[ind], length) + write(1, &buffer[1], i - 1));
+			return (write(1, &buffer[ind], length) + write(1, &padd, 1) * (i - 1));
 		}
 		else if (!(flags & F_MINUS) && padd == ' ') /* extra char to left of buff */
 		{
 			if (extra_c)
 				buffer[--ind] = extra_c;
-			return (write(1, &buffer[1], i - 1) + write(1, &buffer[ind], length));
+			return (write(1, &padd, 1) * (i - 1) + write(1, &buffer[ind], length));
 		}
 		else if (!(flags & F_MINUS) && padd == '0')
 		{
@@ -136,8 +130,8 @@ int write_num(int ind, char buffer[], int flags,
 				buffer[--padd_start] = extra_c;
 			}
 		}
-			return (write(1, &buffer[padd_start], i - padd_start) +
-					write(1, &buffer[ind], length - (1 - padd_start)));
+		return (write(1, &padd, 1) * (i - 1 - (padd == '0')) +
+				write(1, &buffer[ind], length - (padd == '0')));
 	}
 	if (extra_c)
 		buffer[--ind] = extra_c;
